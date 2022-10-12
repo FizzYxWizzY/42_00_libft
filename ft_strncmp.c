d2411ac8fed41fe2cbd0b0d6dc650c91a92f5ffb
FizzY WizzY <102967945+FizzYxWizzY@users.noreply.github.com>
2022-10-12 16:07:07 +0200

    Add files via upload

---
+++ ft_strncmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 16:10:21 by mflury            #+#    #+#             */
+/*   Updated: 2022/10/11 16:10:21 by mflury           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] > s2[i])
+		{
+			return (1);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+/*
+int	main()
+{
+	printf("%d\n", ft_strncmp("Hello", "Heollo 42", 3));
+}
+*/
