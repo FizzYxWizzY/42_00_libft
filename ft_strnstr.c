2c5747ba2b6ecbb7fa6c0f3e4a3ee44ec420288d
FizzY WizzY <102967945+FizzYxWizzY@users.noreply.github.com>
2022-10-25 16:33:42 +0200

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 12:14:46 by mflury            #+#    #+#             */
+/*   Updated: 2022/10/25 16:33:07 by mflury           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+    while (i < len)
+    {
+	    if (to_find[0] == '\0')
+	    {
+		    return ((char *)str);
+	    }
+		j = 0;
+	    while (str[i] != '\0')
+	    {
+		    while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		    {
+			    if (to_find[j + 1] == '\0')
+			    {
+				    return (&((char *)str)[i]);
+			    }
+			    j++;
+		    }
+		    i++;
+	    }
+		break ;
+    }
+	return (0);
+}
+/*
+int	main()
+{
+	char	*str = "putin de merde marche";
+	char	*to_find = "ib";
+
+	printf("%s\n", ft_strnstr(str, to_find, '7'));
+}
+*/
