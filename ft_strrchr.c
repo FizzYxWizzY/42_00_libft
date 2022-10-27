bac5cdca7aee9ca740824e8aa4d3f399ea9a6abb
FizzY WizzY <102967945+FizzYxWizzY@users.noreply.github.com>
2022-10-27 15:06:34 +0200

    Add files via upload

---
+++ ft_strrchr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 14:54:27 by mflury            #+#    #+#             */
+/*   Updated: 2022/10/27 15:06:12 by mflury           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	while (i != 0)
+	{
+		if (s[i] == (char) c)
+			return (&((char *)s)[i]);
+		else 
+			i--;
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	char	*str = "42 lausanne";
+	char	c = ' ';
+	printf ("%s\n", ft_strrchr(str, c));
+	
+}
+*/
