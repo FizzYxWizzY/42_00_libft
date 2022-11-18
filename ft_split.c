fd1090178e3c19b47b212d9d51829147aad6470f
FizzY WizzY <102967945+FizzYxWizzY@users.noreply.github.com>
2022-11-18 19:55:48 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,12 @@
 /*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 13:54:13 by mflury            #+#    #+#             */
-/*   Updated: 2022/11/17 18:08:27 by mflury           ###   ########.fr       */
+/*   Updated: 2022/11/18 19:23:05 by mflury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
 static unsigned int	ft_wcount(const char *s, char c)
 {
 	unsigned int	i;
@@ -34,20 +34,66 @@ static unsigned int	ft_wcount(const char *s, char c)
 	return (count);
 }
 
-static void	ft_wwrite()
+static int	ft_wlen(char *s, unsigned int start, char c)
+{
+	int	i;
+	
+	i = 0;
+	while (s[start] == c)
+		start++;
+	while (s[start])
+	{
+		if (s[start] == c)
+			break ;
+		i++;
+		start++;
+	}
+	return (i);
+}
 
+static void	ft_wwrite(char **tab, const char *s, char c)
+{
+	unsigned int	i;
+	unsigned int	nlist;
+	unsigned int	lastpos;
 
-static
+	i = 0;
+	nlist = 0;
+	lastpos = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+			lastpos = 0;
+		}
+		while (s[i] != c)
+		{
+			tab[nlist][lastpos] = s[i];
+			if (s[i + 1] == c)
+			{
+				tab[nlist][lastpos + 1] = '\0';
+				nlist++;
+				tab[nlist] = malloc(count_word(s, i, c) * sizeof(char));
+			}
+			i++;
+			lastpos++;
+		}
+	}
+}
 
 
 
 
 char	**ft_split(const char *s, char c)
 {
-	char	*tab;
+	char	**tab;
 
+	if (!c)
+		return (s);
 	tab = ft_calloc((ft_wcount(s, c) + 1), sizeof(char *));
-	if (!tab)
+	if (!tab || !s)
 		return (NULL);
-	ft_w
+	
 }
+*/
